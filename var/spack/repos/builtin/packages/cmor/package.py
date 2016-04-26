a82ff527a56fede2a9f26e47e786de779e50e77f
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2016-04-26 19:15:56 -0400

    Add: package cmor from uvcdat

---
+++ var/spack/repos/builtin/packages/cmor/package.py
@@ -0,0 +1,15 @@
+from spack import *
+
+class Cmor(Package):
+    """Climate Model Output Rewriter"""
+
+    homepage = "https://pcmdi.github.io/cmor-site"
+    url      = "https://github.com/UV-CDAT/uvcdat/archive/v2.4.1.tar.gz"
+
+    version('2.4.1', 'e54851295952891dabd0fe3840f59f3b')
+
+    def install(self, spec, prefix):
+        with working_dir('Packages/cmor'):
+            configure("--prefix=%s" % prefix)
+            make()
+            make("install")
