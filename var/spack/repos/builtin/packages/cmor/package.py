c66220d5defb8c74a6db5b3db01d854be55bc824
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2016-04-26 14:15:29 -0400

    build cmor

--- var/spack/repos/builtin/packages/cmor/package.py
+++ var/spack/repos/builtin/packages/cmor/package.py
@@ -8,6 +8,9 @@ class Cmor(Package):
 
     version('2.4.1', 'e54851295952891dabd0fe3840f59f3b')
 
+    depends_on('ossp-uuid')
+    depends_on('udunits2')
+
     def install(self, spec, prefix):
         with working_dir('Packages/cmor'):
             configure("--prefix=%s" % prefix)
