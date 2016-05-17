0ab2464b1ad32fba79feb5c15f387519bb398163
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2016-05-17 15:06:00 -0400

    Add: package py-esmf

--- var/spack/repos/builtin/packages/py-esmf/package.py
+++ var/spack/repos/builtin/packages/py-esmf/package.py
@@ -2,7 +2,7 @@ import os
 from spack import *
 
 
-class Esmf(Package):
+class PyEsmf(Package):
     """
     The Earth System Modeling Framework (ESMF) is a suite of software tools
     for developing high-performance, multi-component Earth science modeling
@@ -16,21 +16,23 @@ class Esmf(Package):
 
     homepage = "https://www.earthsystemcog.org/projects/esmf://www.earthsystemcog.org/projects/esmf/"
 
-    # Direct versions require acceptance of a license
     #url      = "http://www.earthsystemmodeling.org/esmf_releases/non_public/ESMF_7_0_0/esmf_7_0_0_src.tar.gz"
     url      = "http://uvcdat.llnl.gov/cdat/resources/esmp.ESMF_6_3_0rp1_ESMP_01.tar.bz2"
 
     version('6.3.0', 'a9be4fb51da1bc1fab027137297c5030', url="http://uvcdat.llnl.gov/cdat/resources/esmp.ESMF_6_3_0rp1_ESMP_01.tar.bz2")
 
     # TODO: add optional packages
-    extends("python")
+
+    depends_on("esmf")
+    depends_on("python")
     depends_on("py-numpy")
+    extends("python")
 
     def install(self, spec, prefix):
         # TODO: add optional packages
-        os.environ["ESMF_DIR"] = os.path.join(self.stage.source_path, "esmf")
+        os.environ["ESMFMKFILE"] = os.path.join(spec["esmf"], "lib", "esmf.mk")
         os.environ["ESMF_INSTALL_PREFIX"] = prefix
 
-        with working_dir('esmf'):
+        with working_dir('ESMP'):
             make()
             make("install")
