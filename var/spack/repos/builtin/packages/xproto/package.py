d0173a1e839e03b3896d065d629c01df4533323f
Dhanannjay 'Djay' Deo <dhanannjay.deo@kitware.com>
2016-05-18 19:24:45 -0400

    flake8 fixes for xproto package

--- var/spack/repos/builtin/packages/xproto/package.py
+++ var/spack/repos/builtin/packages/xproto/package.py
@@ -4,8 +4,7 @@ from spack import *
 class Xproto(Package):
     """ X11 core wire protocol and auxiliary headers """
     homepage = "http://xorg.freedesktop.org"
-    url      = \
-        "https://www.x.org/archive//individual/proto/xproto-7.0.28.tar.gz"
+    url = "https://www.x.org/archive//individual/proto/xproto-7.0.28.tar.gz"
 
     version('7.0.28', '0b42843b99aee3e4f6a9cc7710143f86')
 
