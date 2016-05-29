fe797f8b3bb26d6460307c2d53467afa1a7ad470
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2016-05-29 14:39:37 -0400

    Add: package cdat_info

---
+++ var/spack/repos/builtin/packages/cdat_info/package.py
@@ -0,0 +1,28 @@
+from spack import *
+
+class CdatInfo(Package):
+    """Climate Model Output Rewriter"""
+
+    homepage = "https://pcmdi.github.io/cmor-site"
+    url      = "https://github.com/UV-CDAT/uvcdat/archive/v2.4.1.tar.gz"
+
+    version('2.4.1', 'e54851295952891dabd0fe3840f59f3b')
+
+    # depends_on('ossp-uuid')
+    # depends_on('udunits2')
+    # depends_on('python')
+
+    extends('python')
+    depends_on('py-pygments')
+    depends_on('py-setuptools')
+
+    def install(self, spec, prefix):
+        with working_dir('Packages/cdat_info'):
+            python('setup.py', 'install', '--prefix=%s' % prefix)
+
+    # def install(self, spec, prefix):
+        # with working_dir('Packages/cdms2'):
+            # configure("--prefix=%s" % prefix,
+                      # "--with-python")
+            # make()
+            # make("install")
